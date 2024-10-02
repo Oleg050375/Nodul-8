b65938dc247e6c52431dd83451c253b3ec8b35e3
Oleg050375 <ov_varganov@mail.ru>
2024-10-02 07:15:56 +0500

    Add files via upload

--- uncorrect.py
+++ uncorrect.py
@@ -1,20 +1,20 @@
 class Car:  # класс автомобилей
     def __init__(self, model, VIN, numbers):
+        self.is_valid_vin(VIN)  # проверка VIN при создании объекта
+        self.is_valid_mumbers(numbers)  # проверка номера при создании объекта
         self.model = model  # название модели (str)
         self.__VIN = VIN  # VIN-номер автомобиля (int) защищённый от переопределения
         self.__numbers = numbers  # номера автомобиля (str) защищённый от переопределения
-        self.is_valid_vin()  # проверка VIN при создании объекта
-        self.is_valid_mumbers()  # проверка номера при создании объекта
-    def is_valid_vin(self):  # метод проверки VIN
-        if not isinstance(self.__VIN, int):  # проверка типа данных VIN
+    def is_valid_vin(self, VIN):  # метод проверки VIN
+        if not isinstance(VIN, int):  # проверка типа данных VIN
             raise IncorrectVinNumber('Некорректный тип VIN номера')  # генерация ошибки типа данных VIN
-        if self.__VIN < 1000000 or self.__VIN > 9999999:  # проверка диапазона VIN
+        if VIN < 1000000 or VIN > 9999999:  # проверка диапазона VIN
             raise IncorrectVinNumber('Неверный диапазон для VIN номера')  # генерация ошибки диапазона VIN
         return True
-    def is_valid_mumbers(self):  # метод проверки номера
-        if not isinstance(self.__numbers, str):  # проверка типа данных номера
+    def is_valid_mumbers(self, numbers):  # метод проверки номера
+        if not isinstance(numbers, str):  # проверка типа данных номера
             raise IncorrectCarNumbers('Некорректный тип данных для номеров')  # генерация ошибки типа данных номера
-        if not len(self.__numbers) == 6:  # проверка длины номера
+        if not len(numbers) == 6:  # проверка длины номера
             raise IncorrectCarNumbers('Неверная длина номера')  # генерация ошибки длины номера
         return True
 
@@ -32,6 +32,7 @@ try:
     z = Car('Zeekr', 7777777, 'a1b2c3')
 except IncorrectVinNumber as exc:
     print(exc.message)
+    #print(z._Car__VIN)
 except IncorrectCarNumbers as exc:
     print(exc.message)
 else:
@@ -54,3 +55,7 @@ except IncorrectCarNumbers as exc:
     print(exc.message)
 else:
     print(f'{b.model} успешно создан')
+
+print(z)  # взглянем на объект z
+#print(a)  # взглянем на объект a
+#print(b)  # взглянем на объект b
